ec3112ab7d166c191821a46ff716189381ff1f0f
interpreterhegel <interpreterhegel@gmail.com>
2024-02-20 19:18:38 +0800

    V1: Interface improved
    Move the comments at the beginning to the end. Remove the exe document.

--- Ampersand from Dev.cpp
+++ Ampersand from Dev.cpp
@@ -1,26 +1,7 @@
-/* 
-1.Knowledge
-Single ampersand & ===> Bit-wise and. 1000 & 1101 = 1010
-Double ampersand && ===> Logical and. True & true = ture. None-zero value = true.
-2.Two ways to load sample data:
-Set a global/local 2-dimensional array for integers. Applied here.
-Define a structure and redefine each time in the loop. Included in the comments but it's not recommended. 
-3.Summary: 
-On 20 Feb 2024 this 50-line c program was written as the first project in Github 
-either as a remainder for myself when meeting similar problems again and an example of my coding.
-Not so much surprise, but happy enough for a freshman here.
-*/
-
 //Comments are not recommended to be copied. They provide a less-efficient alternative.
 #include<stdio.h>
 #include<cstdlib>
 
-/*
-typedef struct test_general{
-	int x;
-	int y;
-}test;
-*/
 
 int test_data [5][2] = {
 	{3,4},
@@ -33,11 +14,7 @@ int test_data [5][2] = {
 int main (){
 	
 	for(int i = 0; i < 5; i ++ ){
-		/*
-		test a;
-		a.x = test_data[i][0];
-		a.y = test_data[i][1];
-		*/
+		
 		printf("Example: x = %d, y = %d\n",test_data[i][0] , test_data[i][1]);
 		printf("x & y = %d\nx && y = %d\n", test_data[i][0] & test_data[i][1], test_data[i][0] && test_data[i][1]);
 	}
@@ -45,6 +22,7 @@ int main (){
 	system("pause");
 	return 0;
 }
+
 
 /*Expected result:
 	x = 3, y = 4;
@@ -64,3 +42,15 @@ int main (){
 	4,1
 */
 
+/* 
+1.Knowledge
+Single ampersand & ===> Bit-wise and. 1000 & 1101 = 1010
+Double ampersand && ===> Logical and. True & true = ture. None-zero value = true.
+2.Two ways to load sample data:
+Set a global/local 2-dimensional array for integers. Applied here.
+Define a structure and redefine each time in the loop. Included in the comments but it's not recommended. 
+3.Summary: 
+On 20 Feb 2024 this 50-line c program was written as the first project in Github 
+either as a remainder for myself when meeting similar problems again and an example of my coding.
+Not so much surprise, but happy enough for a freshman here.
+*/
